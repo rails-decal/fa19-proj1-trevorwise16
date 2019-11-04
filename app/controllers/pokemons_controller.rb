070812db21b276af539f81452bbc353bf11c0f94
Trevor Wise <trevorwise16@berkeley.edu>
2019-11-04 15:21:32 -0800

    finished part 3

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,11 @@
 class PokemonsController < ApplicationController
+  def capture()
+    pokemon = Pokemon.find(params[:id])
+    user = current_trainer
+    pokemon.trainer = user
+    user.pokemons << pokemon
+    pokemon.save
+    user.save
+    redirect_to :root
+  end
 end
